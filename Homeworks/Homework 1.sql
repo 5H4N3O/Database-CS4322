89324e60b597dbe096d07cf49a6a95a1d4e7df5a
5H4N3O <shanepratt2002@gmail.com>
2025-02-06 18:00:52 -0600

    Created basic employees table

---
+++ Homeworks/Homework 1.sql
@@ -0,0 +1,11 @@
+create table employees(
+    firstName varchar(50),
+    middleName varchar(50),
+    lastName varChar(50),
+    eID int,
+    salary int,
+    title varchar(50),
+    primary key (eID)
+);
+
+select * from employees;
