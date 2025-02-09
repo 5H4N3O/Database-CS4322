97fe944333b31bcd8ebd0fae1fbccac03777629c
5H4N3O <shanepratt2002@gmail.com>
2025-02-09 17:36:52 -0600

    Created assignments table and added some columns to other tables

--- Homeworks/Homework 1.sql
+++ Homeworks/Homework 1.sql
@@ -1,34 +1,53 @@
-create table employees(
-    firstName varchar(50),
-    middleName varchar(50),
-    lastName varChar(50),
-    eID int,
-    salary int,
-    title varchar(50),
-    primary key (eID)
-);
-
-create table departments(
-    depNumber int,
-    depName varchar(50),
-    mainOffice varchar(50),
-    officeCount int,
-    assignedEmployees int,
-    managerID int,
-    primary key (depNumber, managerID),
-    foreign key (managerID) references employees (eID)
-);
-
 create table offices(
-    oID int,
+    oID int unique,
     oNumber int,
     floor int,
-    size double precision,
+    size decimal,
     maxOccupancy int,
     primary key (oID)
 );
 
-select * from offices;
+create table employees(
+    firstName varchar(50),
+    middleName varchar(50),
+    lastName varchar(50),
+    jobTitle varchar(50),
+    eID int unique,
+    officeID int,
+    salary int,
+    primary key (eID),
+    foreign key (officeID) references offices (oID)
+);
 
+create table departments(
+    depNumber int unique,
+    depName varchar(50),
+    mainOfficeID int,
+    officeCount int,
+    assignedEmployees int,
+    managerID int unique,
+    adminID int unique,
+    primary key (depNumber),
+    foreign key (managerID) references employees (eID),
+    foreign key (adminID) references employees(eID),
+    foreign key (mainOfficeID) references offices(oID)
+);
+
+create table assignments(
+    employeeID int,
+    weeklyHours int,
+    departmentID int,
+    primary key (employeeID, departmentID),
+    foreign key (employeeID) references employees(eID),
+    foreign key (departmentID) references departments(depNumber)
+);
+
+select * from employees;
 select * from departments;
+select * from offices;
+select * from assignments;
+
+drop table assignments;
 drop table departments;
+drop table employees;
+drop table offices;
