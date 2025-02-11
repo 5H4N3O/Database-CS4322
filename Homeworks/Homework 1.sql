7afe09bd107a78bcd1c376b4a5c897b2443f71c6
5H4N3O <shanepratt2002@gmail.com>
2025-02-11 15:29:14 -0600

    Cleaned up formatting in order to condense lines

--- Homeworks/Homework 1.sql
+++ Homeworks/Homework 1.sql
@@ -1,33 +1,22 @@
 create table offices(
-    oID int unique,
-    oNumber int,
-    floor int,
-    size decimal,
-    maxOccupancy int,
-    primary key (oID)
+    oID int unique, oNumber int,
+    floor int, size decimal,
+    maxOccupancy int, primary key (oID)
 );
 
 create table employees(
-    firstName varchar(50),
-    middleName varchar(50),
-    lastName varchar(50),
-    jobTitle varchar(50),
-    eID int unique,
-    officeID int,
-    salary int,
-    primary key (eID),
+    firstName varchar(50), middleName varchar(50),
+    lastName varchar(50), jobTitle varchar(50),
+    eID int unique, officeID int,
+    salary int, primary key (eID),
     foreign key (officeID) references offices (oID)
 );
 
 create table departments(
-    depNumber int unique,
-    depName varchar(50),
-    mainOfficeID int,
-    officeCount int,
-    assignedEmployees int,
-    managerID int unique,
-    adminID int unique,
-    primary key (depNumber),
+    depNumber int unique, depName varchar(50),
+    mainOfficeID int, officeCount int,
+    assignedEmployees int, managerID int unique,
+    adminID int unique, primary key (depNumber),
     foreign key (managerID) references employees (eID),
     foreign key (adminID) references employees(eID),
     foreign key (mainOfficeID) references offices(oID)
