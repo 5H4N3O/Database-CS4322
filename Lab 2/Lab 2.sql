8e480ce1099b6a9efafd9aa8d29e5fd6d9b88bf6
5H4N3O <shanepratt2002@gmail.com>
2025-02-12 16:59:38 -0600

    Added some selects and created TwinsBatting2 table

--- Lab 2/Lab 2.sql
+++ Lab 2/Lab 2.sql
@@ -1,3 +1,5 @@
+-- create a new version of the three tables
+-- which you can call TwinsPlayerList2, TwinsBatting2, and TwinsFielding2.
 -- You should create these by creating queries
 -- on the relations master, batting, and fielding
 -- Extra fields in batting anf fielding
@@ -5,7 +7,18 @@
 -- Only need stuff from Twins from 2010 to 2015
 -- all data for playerlist is in master
 -- create table as (query)
-select *
-from public.batting;
+--
+
+select * from public.master;
+select * from public.batting where yearid >= 2010 and yearid <= 2015 and teamid = 'MIN';
+select * from public.fielding;
 
 create table pratt440.test();
+
+create table pratt440.TwinsBatting2 as
+    select playerid
+    from public.batting
+    where yearid < 2010
+    ;
+
+select * from pratt440.TwinsBatting2;
