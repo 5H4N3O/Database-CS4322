1fdb4c5488adffe6b516206122458f7a8dd26aff
5H4N3O <shanepratt2002@gmail.com>
2025-02-12 16:37:30 -0600

    Added some notes from TA

--- Lab 2/Lab 2.sql
+++ Lab 2/Lab 2.sql
@@ -1,7 +1,11 @@
 -- You should create these by creating queries
 -- on the relations master, batting, and fielding
-
+-- Extra fields in batting anf fielding
+-- Need to specify fields directly
+-- Only need stuff from Twins from 2010 to 2015
+-- all data for playerlist is in master
+-- create table as (query)
 select *
-from batting;
+from public.batting;
 
 create table pratt440.test();
