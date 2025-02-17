6d1c8e3d9724b01ceb94f05e0ae3c6acb013aad9
5H4N3O <shanepratt2002@gmail.com>
2025-02-17 16:31:49 -0600

    Created tempMaster table from master and used it to make TwinsBatting2 foreign key

--- Lab 2/Lab 2.sql
+++ Lab 2/Lab 2.sql
@@ -13,12 +13,38 @@ select * from public.master;
 select * from public.batting where yearid >= 2010 and yearid <= 2015 and teamid = 'MIN';
 select * from public.fielding;
 
-create table pratt440.test();
+create table pratt440.tempMaster as
+    select *
+    from public.master;
+alter table pratt440.tempMaster
+add primary key (playerid);
 
-create table pratt440.TwinsBatting2 as
-    select playerid
-    from public.batting
-    where yearid < 2010
-    ;
+select * from pratt440.tempMaster;
+drop table pratt440.tempMaster;
+
+create table pratt440.TwinsBatting2 (
+	playerid varchar(50) references pratt440.tempMaster(playerid),
+	yearid int, stint int, ab int, r int, h int, db int,
+	tr int, hr int, rbi int, sb int, cs int, bb int, so int, ibb int,
+	hbp int, primary key (playerid, yearid, stint)
+);
+
+insert into pratt440.TwinsBatting2
+	(playerid, yearid, stint, ab, r, h, db, tr, hr, rbi, sb, cs, bb,
+	so, ibb, hbp)
+select
+	distinct playerid, yearid, stint, ab, r, h, db, tr, hr, rbi, sb,
+	cs, bb, so, ibb, hbp
+from public.batting
+where yearid >= 2010 and yearid <= 2015 and teamid = 'MIN';
+
+-- create table pratt440.TwinsBatting2 as
+--     select
+--         distinct playerid, yearid, stint, ab, r, h,
+--         db, tr, hr, rbi, sb, cs, bb, so, ibb, hbp
+--     from public.batting
+--     where yearid >= 2010 and yearid <= 2015 and teamid = 'MIN'
+--     ;
 
 select * from pratt440.TwinsBatting2;
+drop table pratt440.TwinsBatting2;
