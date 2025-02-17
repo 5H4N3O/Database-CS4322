a47f6a7de7e189482847cab0fc8b4689186cb385
5H4N3O <shanepratt2002@gmail.com>
2025-02-17 16:48:52 -0600

    Added separation comments and initial creation of TwinsFielding2

--- Lab 2/Lab 2.sql
+++ Lab 2/Lab 2.sql
@@ -13,19 +13,35 @@ select * from public.master;
 select * from public.batting where yearid >= 2010 and yearid <= 2015 and teamid = 'MIN';
 select * from public.fielding;
 
+-- TwinsBatting2
 create table pratt440.TwinsBatting2 as
     select
         distinct playerid, yearid, stint, ab, r, h,
         db, tr, hr, rbi, sb, cs, bb, so, ibb, hbp
     from public.batting
     where yearid >= 2010 and yearid <= 2015 and teamid = 'MIN';
+
 alter table pratt440.TwinsBatting2
-add primary key (playerid,yearid, stint);
+    add primary key (playerid,yearid, stint);
 
 select * from pratt440.TwinsBatting2;
+drop table pratt440.TwinsBatting2;
 
 select playerid
 from pratt440.TwinsBatting2
 where playerid = 'achteaj01';
+-- TwinsBatting2
+
+-- TwinsFielding2
+    create table pratt440.TwinsFielding2 as
+    select
+        distinct playerid, yearid, stint, pos, g,
+        innouts, po, a, e, dp, pb, wp, csb, ccs
+    from public.fielding
+    where yearid >= 2010 and yearid <= 2015 and teamid = 'MIN';
+
+alter table pratt440.TwinsFielding2
+    add primary key (playerid, yearid, stint, pos);
+-- TwinsFielding2
 
 drop table pratt440.TwinsBatting2;
