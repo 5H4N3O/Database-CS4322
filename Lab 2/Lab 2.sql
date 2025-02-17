7aef78a65e57aefca2af163c1e1cb0c3a685daca
5H4N3O <shanepratt2002@gmail.com>
2025-02-17 16:42:01 -0600

    Removed unnecessary queries

--- Lab 2/Lab 2.sql
+++ Lab 2/Lab 2.sql
@@ -13,38 +13,19 @@ select * from public.master;
 select * from public.batting where yearid >= 2010 and yearid <= 2015 and teamid = 'MIN';
 select * from public.fielding;
 
-create table pratt440.tempMaster as
-    select *
-    from public.master;
-alter table pratt440.tempMaster
-add primary key (playerid);
-
-select * from pratt440.tempMaster;
-drop table pratt440.tempMaster;
-
-create table pratt440.TwinsBatting2 (
-	playerid varchar(50) references pratt440.tempMaster(playerid),
-	yearid int, stint int, ab int, r int, h int, db int,
-	tr int, hr int, rbi int, sb int, cs int, bb int, so int, ibb int,
-	hbp int, primary key (playerid, yearid, stint)
-);
-
-insert into pratt440.TwinsBatting2
-	(playerid, yearid, stint, ab, r, h, db, tr, hr, rbi, sb, cs, bb,
-	so, ibb, hbp)
-select
-	distinct playerid, yearid, stint, ab, r, h, db, tr, hr, rbi, sb,
-	cs, bb, so, ibb, hbp
-from public.batting
-where yearid >= 2010 and yearid <= 2015 and teamid = 'MIN';
-
--- create table pratt440.TwinsBatting2 as
---     select
---         distinct playerid, yearid, stint, ab, r, h,
---         db, tr, hr, rbi, sb, cs, bb, so, ibb, hbp
---     from public.batting
---     where yearid >= 2010 and yearid <= 2015 and teamid = 'MIN'
---     ;
+create table pratt440.TwinsBatting2 as
+    select
+        distinct playerid, yearid, stint, ab, r, h,
+        db, tr, hr, rbi, sb, cs, bb, so, ibb, hbp
+    from public.batting
+    where yearid >= 2010 and yearid <= 2015 and teamid = 'MIN';
+alter table pratt440.TwinsBatting2
+add primary key (playerid,yearid, stint);
 
 select * from pratt440.TwinsBatting2;
+
+select playerid
+from pratt440.TwinsBatting2
+where playerid = 'achteaj01';
+
 drop table pratt440.TwinsBatting2;
