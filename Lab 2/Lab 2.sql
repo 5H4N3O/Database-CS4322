4f99860d8e26e5a2b62d29b6b1b03ee50f640e6d
5H4N3O <shanepratt2002@gmail.com>
2025-02-17 17:25:16 -0600

    Removed unnecessary queries

--- Lab 2/Lab 2.sql
+++ Lab 2/Lab 2.sql
@@ -7,11 +7,6 @@
 -- Only need stuff from Twins from 2010 to 2015
 -- all data for playerlist is in master
 -- create table as (query)
---
-
-select * from public.master;
-select * from public.batting where yearid >= 2010 and yearid <= 2015 and teamid = 'MIN';
-select * from public.fielding;
 
 -- TwinsBatting2
 create table pratt440.TwinsBatting2 as
@@ -26,10 +21,6 @@ alter table pratt440.TwinsBatting2
 
 select * from pratt440.TwinsBatting2;
 drop table pratt440.TwinsBatting2;
-
-select playerid
-from pratt440.TwinsBatting2
-where playerid = 'achteaj01';
 -- TwinsBatting2
 
 -- TwinsFielding2
