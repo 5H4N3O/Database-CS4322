3e60365f14c6c571fc1f21433581c044a44de27b
5H4N3O <shanepratt2002@gmail.com>
2025-02-17 17:22:58 -0600

    Added subquery to make sure playerid is present in TwinsBatting2 and TwinsFielding2

--- Lab 2/Lab 2.sql
+++ Lab 2/Lab 2.sql
@@ -47,11 +47,22 @@ alter table pratt440.TwinsFielding2
 -- TwinsFielding2
 
 drop table pratt440.TwinsBatting2;-- TwinsPlayerList2
+-- TwinsPlayerList2
 create table pratt440.TwinsPlayerlist2 as
     select
 	    distinct playerid, namelast, namefirst, namegiven, birthyear,
 	    birthmonth, birthday, weight, height, bats, throws
     from public.master
+    where playerid in(-- Checks if playerid is within TwinsBatting2 or TwinsFielding2
+        select F.playerid
+        from TwinsFielding2 F
+        union
+        select B.playerid
+        from TwinsBatting2 B);
+
+alter table pratt440.TwinsPlayerlist2
+    add primary key (playerid);
+
 select * from pratt440.TwinsPlayerlist2;
 drop table pratt440.TwinsPlayerlist2;
 -- TwinsPlayerList2
